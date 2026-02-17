fe8ef667029c40f55578e09942fc9107c9530197
telome <>
2026-02-17 16:35:19 +0400

    Make fund permissionless & remove redundant view func

--- src/NFATRedeemer.sol
+++ src/NFATRedeemer.sol
@@ -38,9 +38,7 @@ contract NFATRedeemer {
 
     // --- Access Control Storage ---
 
-    mapping(address usr => uint256 allowed)   public wards;
-    mapping(address usr => bytes32 rolesData) public userRoles;
-    mapping(bytes4  sig => bytes32 rolesData) public actionsRoles;
+    mapping(address usr => uint256 allowed) public wards;
     bool public stopped;
 
     // --- Redeem Storage ---
@@ -51,8 +49,6 @@ contract NFATRedeemer {
 
     event Rely(address indexed usr);
     event Deny(address indexed usr);
-    event SetUserRole(address indexed who, uint8 indexed role, bool enabled);
-    event SetRoleAction(uint8 indexed role, bytes4 sig, bool enabled);
     event Stop();
     event Start();
 
@@ -65,15 +61,6 @@ contract NFATRedeemer {
 
     modifier auth() {
         require(wards[msg.sender] == 1, "NFATRedeemer/not-authorized");
-        _;
-    }
-
-    modifier roleAuth() {
-        require(
-            userRoles[msg.sender] & actionsRoles[msg.sig] != bytes32(0) ||
-            wards[msg.sender] == 1,
-            "NFATRedeemer/role-not-authorized"
-        );
         _;
     }
 
@@ -91,7 +78,7 @@ contract NFATRedeemer {
         emit Rely(msg.sender);
     }
 
-    // --- Access Control Functions ---
+    // --- Admin Functions ---
 
     function rely(address usr) external auth {
         wards[usr] = 1;
@@ -101,26 +88,6 @@ contract NFATRedeemer {
     function deny(address usr) external auth {
         wards[usr] = 0;
         emit Deny(usr);
-    }
-
-    function setUserRole(address who, uint8 role, bool enabled) external auth {
-        bytes32 mask = bytes32(uint256(1) << role);
-        if (enabled) {
-            userRoles[who] |= mask;
-        } else {
-            userRoles[who] &= ~mask;
-        }
-        emit SetUserRole(who, role, enabled);
-    }
-
-    function setRoleAction(uint8 role, bytes4 sig, bool enabled) external auth {
-        bytes32 mask = bytes32(uint256(1) << role);
-        if (enabled) {
-            actionsRoles[sig] |= mask;
-        } else {
-            actionsRoles[sig] &= ~mask;
-        }
-        emit SetRoleAction(role, sig, enabled);
     }
 
     function stop() external auth {
@@ -135,10 +102,10 @@ contract NFATRedeemer {
 
     // --- Redeem Functions ---
 
-    /// @notice Keeper deposits funds for NFAT redemption on behalf of Halo
+    /// @notice Deposit funds for NFAT redemption
     /// @param tokenId The NFAT to fund
     /// @param amount The amount of sUSDS to deposit
-    function fund(uint256 tokenId, uint256 amount) external roleAuth notStopped {
+    function fund(uint256 tokenId, uint256 amount) external notStopped {
         require(facility.ownerOf(tokenId) != address(0), "NFATRedeemer/invalid-token");
         require(amount > 0, "NFATRedeemer/zero-amount");
 
@@ -170,28 +137,4 @@ contract NFATRedeemer {
         emit Redeem(tokenId, amount);
     }
 
-    // --- View Functions ---
-
-    /// @notice Get the funded amount for an NFAT
-    /// @param tokenId The NFAT to query
-    /// @return The funded amount
-    function getFunded(uint256 tokenId) external view returns (uint256) {
-        return funded[tokenId];
-    }
-
-    /// @notice Check if a user has a specific role
-    /// @param usr The address to check
-    /// @param role The role ID
-    /// @return has Whether the user has the role
-    function hasUserRole(address usr, uint8 role) external view returns (bool has) {
-        has = userRoles[usr] & bytes32(uint256(1) << role) != bytes32(0);
-    }
-
-    /// @notice Check if an action is assigned to a role
-    /// @param sig The function signature
-    /// @param role The role ID
-    /// @return has Whether the action is in the role
-    function isActionInRole(bytes4 sig, uint8 role) external view returns (bool has) {
-        has = actionsRoles[sig] & bytes32(uint256(1) << role) != bytes32(0);
-    }
 }
